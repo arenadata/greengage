8089b20e575cabdde12d3f66c56d8b84c8910e79
Georgy Shelkovy <g.shelkovy@arenadata.io>
2026-01-19 14:36:13 +0500

    fix

--- gpMgmt/test/behave_utils/ci/fixtures.py
+++ gpMgmt/test/behave_utils/ci/fixtures.py
@@ -5,7 +5,7 @@ from behave import fixture
 def init_cluster(context):
     context.execute_steps(u"""
     Given the database is not running
-        And a working directory of the test as '/tmp/concourse_cluster'
+        And a working directory of the test as '/data/gpdata'
         And the user runs command "rm -rf ~/gpAdminLogs/gpinitsystem*"
         And a cluster is created with mirrors on "cdw" and "sdw1, sdw2, sdw3, sdw4"
     """)
