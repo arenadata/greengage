68a596ad8b4edb1600ec2d60ede35aadcdb295c1
Georgy Shelkovy <g.shelkovy@arenadata.io>
2026-01-20 09:51:27 +0500

    fix

--- ci/scripts/behave_gpdb.bash
+++ ci/scripts/behave_gpdb.bash
@@ -13,7 +13,7 @@ function gen_env(){
 
 		source /usr/local/greengage-db-devel/greengage_path.sh
 
-		source gpdb_src/gpAux/gpdemo/gpdemo-env.sh
+		test -f gpdb_src/gpAux/gpdemo/gpdemo-env.sh && source gpdb_src/gpAux/gpdemo/gpdemo-env.sh
 
 		cd "\${1}/gpdb_src/gpMgmt/"
 		BEHAVE_TAGS="${BEHAVE_TAGS}"
