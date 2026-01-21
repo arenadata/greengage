7f39c3ec3a705fcce6f2efb03327e21c54e06b9c
Georgy Shelkovy <g.shelkovy@arenadata.io>
2026-01-21 12:35:42 +0500

    optimize

--- gpMgmt/bin/lib/gpcreateseg.sh
+++ gpMgmt/bin/lib/gpcreateseg.sh
@@ -184,11 +184,13 @@ CREATE_QES_PRIMARY () {
         fi
     else # use hostnames in pg_hba.conf
         # add localhost
-        MASTER_IP="$($TRUSTED_SHELL ${GP_HOSTADDRESS} "host \"$MASTER_HOSTNAME\" | grep 'has address' | head -n 1 | cut -d ' ' -f 4")"
-        ANOTHER_MASTER_HOSTNAME="$($TRUSTED_SHELL ${GP_HOSTADDRESS} "host \"$MASTER_IP\" | grep 'domain name pointer' | head -n 1 | cut -d ' ' -f 5 | sed 's|\.$||'")"
         $TRUSTED_SHELL ${GP_HOSTADDRESS} "$ECHO host     all          all         localhost      trust >> ${GP_DIR}/$PG_HBA"
         $TRUSTED_SHELL ${GP_HOSTADDRESS} "$ECHO host	all	all	${MASTER_HOSTNAME}	trust >> ${GP_DIR}/$PG_HBA"
-        $TRUSTED_SHELL ${GP_HOSTADDRESS} "$ECHO host	all	all	${ANOTHER_MASTER_HOSTNAME}	trust >> ${GP_DIR}/$PG_HBA"
+        MASTER_IP="$($TRUSTED_SHELL ${GP_HOSTADDRESS} "host \"$MASTER_HOSTNAME\" | grep 'has address' | head -n 1 | cut -d ' ' -f 4")"
+        ANOTHER_MASTER_HOSTNAME="$($TRUSTED_SHELL ${GP_HOSTADDRESS} "host \"$MASTER_IP\" | grep 'domain name pointer' | head -n 1 | cut -d ' ' -f 5 | sed 's|\.$||'")"
+        if [[ -n "$ANOTHER_MASTER_HOSTNAME" && "$ANOTHER_MASTER_HOSTNAME" != "$MASTER_HOSTNAME" ]]; then
+          $TRUSTED_SHELL ${GP_HOSTADDRESS} "$ECHO host	all	all	${ANOTHER_MASTER_HOSTNAME}	trust >> ${GP_DIR}/$PG_HBA"
+        fi
         if [ x"" != x"$MIRROR_HOSTADDRESS" ]; then
           $TRUSTED_SHELL ${GP_HOSTADDRESS} "$ECHO host     all          $USER_NAME         $MIRROR_HOSTADDRESS      trust >> ${GP_DIR}/$PG_HBA"
         fi
