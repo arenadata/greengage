c86d35442c18f9756983bd2116ca1d1974e2d42b
Georgy Shelkovy <g.shelkovy@arenadata.io>
2026-02-03 10:18:52 +0500

    fix

--- ci/scripts/init_containers.sh
+++ ci/scripts/init_containers.sh
@@ -15,21 +15,18 @@ fi
 
 for service in $services
 do
-  docker compose -p "$project" -f ci/docker-compose.yaml exec -T "$service" bash -ex & <<EOF
+  docker compose -p "$project" -f ci/docker-compose.yaml exec -T "$service" bash -exc "
     # Copy /home/gpadmin/.ssh
-    cp -rf /home/gpadmin/.ssh.src /home/gpadmin/.ssh
+    cp -rf /home/gpadmin/.ssh.src /home/gpadmin/.ssh;
     # Prepare ALL containers first
-    mkdir -p /data/gpdata
-    chmod -R 777 /data
-    source gpdb_src/concourse/scripts/common.bash
-    install_gpdb
-    ./gpdb_src/concourse/scripts/setup_gpadmin_user.bash
+    mkdir -p /data/gpdata;
+    chmod -R 777 /data;
+    source gpdb_src/concourse/scripts/common.bash;
+    install_gpdb;
+    ./gpdb_src/concourse/scripts/setup_gpadmin_user.bash;
     # Add host keys to known_hosts after containers setup
-    ssh-keyscan ${services/$service/} >> /home/gpadmin/.ssh/known_hosts
+    ssh-keyscan ${services/$service/} >> /home/gpadmin/.ssh/known_hosts;
     # Add ip and host to /etc/hosts
-    for HOST in \"cdw sdw1 sdw2 sdw3 sdw4 sdw5 sdw6\"; do
-      echo \"\$(host \"\$HOST\" | grep 'has address' | head -n 1 | cut -d ' ' -f 4) \$HOST\" >>/etc/hosts
-    done
-EOF
+    for HOST in \"cdw sdw1 sdw2 sdw3 sdw4 sdw5 sdw6\"; do echo \"\$(host \"\$HOST\" | grep 'has address' | head -n 1 | cut -d ' ' -f 4) \$HOST\" >>/etc/hosts; done" &
 done
 wait
